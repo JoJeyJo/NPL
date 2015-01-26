3f2a3db436c95e5903c19b99236ab6721fbbd6c3
PedroGFonseca <pedrogabrielfonseca@gmail.com>
2015-01-26 14:56:09 +0000

    Data imports from S3, turns into pandas dataframe

--- NPL.py
+++ NPL.py
@@ -1,12 +1,22 @@
 # import libraries 
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+import numpy as np
+import pylab as P
+import csv
+import urllib2
 
-
-# import data
+# import sample data from S3 
+url = 'https://trello-attachments.s3.amazonaws.com/54522d0bd5a9e7596679dd06/54c620fa57b111af46716e5c/a79bc405e58353ef79e2bbbf9adc3290/Mock_data_LGD_-_Sheet1.csv'
+response = urllib2.urlopen(url)
 
 # convert csv data to pandas dataframe 
+df = pd.read_csv(response, header=0)
 
 # clean data:
 #   remove null variables (should not have much, might not be worth using averages)
+
+
 #   make categorical data numerical 
 #   normalize data 
 #   drop non-necessary data
