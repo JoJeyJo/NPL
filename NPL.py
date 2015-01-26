f5d320e555a3e24053a7e4715887f73844698050
PedroGFonseca <pedrogabrielfonseca@gmail.com>
2015-01-26 14:20:23 +0000

    Create NPL.py

---
+++ NPL.py
@@ -0,0 +1,15 @@
+# import libraries 
+
+# import data
+
+# convert csv data to pandas dataframe 
+
+# clean data:
+#   remove null variables (should not have much, might not be worth using averages)
+#   make categorical data numerical 
+#   normalize data 
+#   drop non-necessary data
+
+# convert data to numpy array 
+
+# separate data into training and test sets 
